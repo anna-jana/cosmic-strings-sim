99a4ac1e894fa6bd64e8a9715fbc48d29513062b
Anna-Jana Riess <j.riess@wtnet.de>
2023-01-31 23:55:41 +0100

    some plotting but zeta is not right

---
+++ plot_string_length.py
@@ -0,0 +1,28 @@
+import numpy as np, matplotlib.pyplot as plt
+import prototype_string_detection, cosmology
+
+strings = np.loadtxt("strings.dat")
+step, string_id, x, y, z = strings.T
+
+steps = []
+lengths = []
+
+for step_used in np.unique(step):
+    for i in np.unique(string_id[step == step_used]):
+        length = np.sum((step == step_used) & (string_id == i)) * 3/2 * prototype_string_detection.dx
+        steps.append(step_used)
+        lengths.append(length)
+
+tau = cosmology.tau_start + np.array(steps) * cosmology.dtau
+log = cosmology.tau_to_log(tau)
+
+L = 1.0
+t = cosmology.tau_to_t(tau)
+zeta = np.array(lengths) / L**3 * t**2
+
+# I think this is not right
+
+plt.scatter(log, zeta)
+plt.xlabel(r"$\log(m_r / H)")
+plt.ylabel(r"$\zeta = l t^2 / L^3$")
+plt.show()
--- plot_strings.py
+++ plot_strings.py
@@ -5,7 +5,7 @@ strings = np.loadtxt("strings.dat")
 step, string_id, x, y, z = strings.T
 string_list = []
 step_used = np.unique(step)[-1]
-for i in np.unique(string_id):
+for i in np.unique(string_id[step == step_used]):
     mask = (step == step_used) & (string_id == i)
     string_list.append(list(zip(x[mask], y[mask], z[mask])))
 prototype_string_detection.plot(string_list, prototype_string_detection.N)
