7e629b76e2f95ef5c392d49bd63ac588c4439de1
Anna-Jana Riess <j.riess@wtnet.de>
2023-11-19 00:23:46 +0100

    writeup for string profile

--- string_solution.jl
+++ string_solution.jl
@@ -2,9 +2,9 @@ using DifferentialEquations
 using PyPlot
 
 function rhs!(dy, y, p, u)
-    r, dr = y
-    dy[1] = dr
-    dy[2] = + dr + r + exp(u)^2 * r * (r^2 - 1)
+    f, df = y
+    dy[1] = df
+    dy[2] = + df + f + exp(u)^2 * f * (f^2 - 1)
 end
 
 function bc_left!(residual, y, p)
@@ -15,7 +15,7 @@ function bc_right!(residual, y, p)
     residual[1] = y[1]
 end
 
-u_min = log(1e-5) # u = log(r)
+u_min = log(1e-5) # u = log(rho)
 u_max = log(5)
 initial_guess = [0.0, 1.0]
 npoints = 500
@@ -24,12 +24,13 @@ problem = TwoPointBVProblem(rhs!, (bc_left!, bc_right!),
                             bcresid_prototype = (zeros(1), zeros(1)))
 solution = solve(problem, MIRK4(), dt = u_max / npoints)
 rho = exp.(solution.t)
-r = solution[1, :]
+f = solution[1, :]
+r_over_f_a = @. 1 - f
 
 figure()
-semilogx(rho, r)
-xlabel("radial distance to string")
-ylabel(raw"radial component of PQ field $r(\rho)$")
+semilogx(rho, r_over_f_a)
+xlabel(raw"radial distance to string $\rho$")
+ylabel(raw"dimensionless radial component $r / f_a$ of PQ field $\frac{r + f_a}{\sqrt{2}} e^{i \theta}$")
 title("single string")
 savefig("string_solution.pdf")
 show()
