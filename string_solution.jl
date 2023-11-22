db467b2b9845e0faebcee1dfc8dd7c8fd71aac8f
Anna-Jana Riess <j.riess@wtnet.de>
2023-11-22 15:51:45 +0100

    fixed numerical solution bc

--- string_solution.jl
+++ string_solution.jl
@@ -8,15 +8,15 @@ function rhs!(dy, y, p, u)
 end
 
 function bc_left!(residual, y, p)
-    residual[1] = y[1] - one(y[1])
-end
-
-function bc_right!(residual, y, p)
     residual[1] = y[1]
 end
 
+function bc_right!(residual, y, p)
+    residual[1] = y[1] - one(y[1])
+end
+
 u_min = log(1e-5) # u = log(rho)
-u_max = log(5)
+u_max = log(10)
 initial_guess = [0.0, 1.0]
 npoints = 500
 problem = TwoPointBVProblem(rhs!, (bc_left!, bc_right!),
@@ -28,7 +28,7 @@ f = solution[1, :]
 r_over_f_a = @. 1 - f
 
 figure()
-semilogx(rho, r_over_f_a)
+plot(rho, r_over_f_a)
 xlabel(raw"radial distance to string $\rho$")
 ylabel(raw"dimensionless radial component $r / f_a$ of PQ field $\frac{r + f_a}{\sqrt{2}} e^{i \theta}$")
 title("single string")
